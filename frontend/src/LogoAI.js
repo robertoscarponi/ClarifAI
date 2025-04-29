08771175487e212ad26bd51b837f1120cb90871b
Pietro Tognasca <pietro.tognasca@mail.polimi.it>
2025-04-29 11:13:50 +0200

    Modificato il logo provvisorio

--- frontend/src/LogoAI.js
+++ frontend/src/LogoAI.js
@@ -7,34 +7,43 @@ const ClarifAILogo = ({ className }) => (
     xmlns="http://www.w3.org/2000/svg"
     className={className}
   >
-    {/* Arco superiore: rosso-arancio */}
+    {/* Elemento curvo superiore (rosso-arancione) */}
     <path
-      d="M100,40
-         C135,35 160,65 150,100
-         C145,120 120,130 100,120"
-      fill="none"
-      stroke="url(#grad1)"
-      strokeWidth="28"
-      strokeLinecap="round"
+      d="M100,30 
+         C140,30 170,60 170,100
+         C170,115 165,125 155,135
+         C145,145 130,152 115,152
+         C105,152 95,148 85,140
+         C75,132 68,122 65,110"
+      fill="url(#grad1)"
+      stroke="none"
     />
-    {/* Arco inferiore: azzurro-blu */}
+    
+    {/* Elemento curvo inferiore (blu-turchese) */}
     <path
-      d="M100,160
-         C65,165 40,135 50,100
-         C55,80 80,70 100,80"
-      fill="none"
-      stroke="url(#grad2)"
-      strokeWidth="28"
-      strokeLinecap="round"
+      d="M100,170
+         C60,170 30,140 30,100
+         C30,85 35,75 45,65
+         C55,55 70,48 85,48
+         C95,48 105,52 115,60
+         C125,68 132,78 135,90"
+      fill="url(#grad2)"
+      stroke="none"
     />
+    
     <defs>
+      {/* Gradiente per la parte rossa-arancione */}
       <linearGradient id="grad1" x1="60" y1="40" x2="160" y2="120" gradientUnits="userSpaceOnUse">
-        <stop stopColor="#F2994A"/>
-        <stop offset="1" stopColor="#E15A46"/>
+        <stop offset="0%" stopColor="#F24C3D" />
+        <stop offset="50%" stopColor="#F5762A" />
+        <stop offset="100%" stopColor="#F9A826" />
       </linearGradient>
+      
+      {/* Gradiente per la parte blu-turchese */}
       <linearGradient id="grad2" x1="140" y1="160" x2="40" y2="80" gradientUnits="userSpaceOnUse">
-        <stop stopColor="#48A1B0"/>
-        <stop offset="1" stopColor="#274676"/>
+        <stop offset="0%" stopColor="#22A699" />
+        <stop offset="50%" stopColor="#5FBDBB" />
+        <stop offset="100%" stopColor="#7ED7C1" />
       </linearGradient>
     </defs>
   </svg>
